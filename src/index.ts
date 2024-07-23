6ad91fcd36d1bafc03880fb61c89f46a58f3f210
Huy Dinh <madhattervn@gmail.com>
2024-07-23 12:02:20 +0700

    fix: cookie auth

--- src/index.ts
+++ src/index.ts
@@ -86,10 +86,8 @@ const main = async () => {
 			cookie: {
 				maxAge: 1000 * 60 * 60, // one hour
 				httpOnly: true, // JS front end cannot access the cookie
-				// secure: __prod__, // cookie only works in https
-				secure: false, // cookie only works in https
-				// sameSite: "lax",
-				// domain: __prod__ ? "swipeit-website.vercel.app" : undefined,
+				secure: __prod__, // cookie only works in https
+				sameSite: "none",
 			},
 			secret: process.env.SESSION_SECRET_DEV_PROD as string,
 			saveUninitialized: false, // don't save empty sessions, right from the start
